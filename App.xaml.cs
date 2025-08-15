b759b58d876b95061cda6dd5baeaed7b81d077bf
Turara_Hoshikoma <yuuma_23s1103129@nnn.ed.jp>
2025-08-15 18:58:24 +0900

    UI制作の下準備

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Tatehama_tetudou_denwa_PCclient;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
