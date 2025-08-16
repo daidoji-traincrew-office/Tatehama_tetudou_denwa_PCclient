dda0029aa536ecd1eb639430a152f8f93080c739
Turara_Hoshikoma <yuuma_23s1103129@nnn.ed.jp>
2025-08-16 19:00:19 +0900

    UIズレを修正

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,23 +1,90 @@
-﻿using System.Text;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Tatehama_tetudou_denwa_PCclient;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
+    private enum PhoneState
+    {
+        Idle,
+        OffHook,
+        Dialing,
+        InCall
+    }
+
+    private PhoneState currentState = PhoneState.Idle;
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void AppendNumber(string number)
+    {
+        if (currentState == PhoneState.OffHook)
+        {
+            NumberDisplay.Text += number;
+        }
+    }
+
+    private void syoukyoButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (currentState == PhoneState.OffHook && NumberDisplay.Text.Length > 0)
+        {
+            NumberDisplay.Text = NumberDisplay.Text.Substring(0, NumberDisplay.Text.Length - 1);
+        }
+    }
+
+    private void zeroButton_Click(object sender, RoutedEventArgs e) { AppendNumber("0"); }
+    private void oneButton_Click(object sender, RoutedEventArgs e) { AppendNumber("1"); }
+    private void twoButton_Click(object sender, RoutedEventArgs e) { AppendNumber("2"); }
+    private void threeButton_Click(object sender, RoutedEventArgs e) { AppendNumber("3"); }
+    private void fourButton_Click(object sender, RoutedEventArgs e) { AppendNumber("4"); }
+    private void fiveButton_Click(object sender, RoutedEventArgs e) { AppendNumber("5"); }
+    private void sixButton_Click(object sender, RoutedEventArgs e) { AppendNumber("6"); }
+    private void sevenButton_Click(object sender, RoutedEventArgs e) { AppendNumber("7"); }
+    private void eightButton_Click(object sender, RoutedEventArgs e) { AppendNumber("8"); }
+    private void nineButton_Click(object sender, RoutedEventArgs e) { AppendNumber("9"); }
+
+    private void jyuwaButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (currentState == PhoneState.Idle)
+        {
+            currentState = PhoneState.OffHook;
+            jyuwaButton.Background = new ImageBrush(new BitmapImage(new Uri("/image/jyuwa-aka.png", UriKind.Relative))) { Stretch = Stretch.Fill };
+            syuwaButton.Background = new ImageBrush(new BitmapImage(new Uri("/image/syuwa-ao.png", UriKind.Relative))) { Stretch = Stretch.Fill };
+        }
+    }
+
+    private void syuwaButton_Click(object sender, RoutedEventArgs e)
+    {
+        currentState = PhoneState.Idle;
+        NumberDisplay.Text = "";
+        jyuwaButton.Background = new ImageBrush(new BitmapImage(new Uri("/image/jyuwa.png", UriKind.Relative))) { Stretch = Stretch.Fill };
+        syuwaButton.Background = new ImageBrush(new BitmapImage(new Uri("/image/syuwa.png", UriKind.Relative))) { Stretch = Stretch.Fill };
+        hashinButton.Background = new ImageBrush(new BitmapImage(new Uri("/image/hashin.png", UriKind.Relative))) { Stretch = Stretch.Fill };
+    }
+
+    private void hashinButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (currentState == PhoneState.OffHook && !string.IsNullOrEmpty(NumberDisplay.Text))
+        {
+            currentState = PhoneState.Dialing;
+            hashinButton.Background = new ImageBrush(new BitmapImage(new Uri("/image/hashin-aka.png", UriKind.Relative))) { Stretch = Stretch.Fill };
+        }
+    }
+
+    private void ChangeWorkLocationMenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        // TODO: Implement logic
+    }
+
+    private void SettingsMenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        // TODO: Implement logic
     }
 }
